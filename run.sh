453d93d33fbd91ba1b70eba60d55073a6327cb92
sotheara <leangsotheara@gmail.com>
2019-09-02 15:14:59 +0700

    commit codes

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-log(){
-    echo $1
-}
-
 # initialization PATH
 . ./path.sh || exit 1
 
@@ -11,11 +7,10 @@ log(){
 . ./cmd.sh || exit 1
 
 # parameters
-
 data_dir=$1
 output_dir=$2
 step=$3
-nj=$4 # number of parallel jobs - 1 is perfect for such a small dataset
+nj=$4
 
 if [[ -z $data_dir ]]; then
     data_dir=data
@@ -33,11 +28,18 @@ if [[ -z $nj ]]; then
     nj=4
 fi
 
-#### Stage 1 - Data preparation ####
+# load configuration
+. conf/run.conf
+
+#### Step 1 - Data preparation ####
 
 if [[ $step -eq 1 ]] || [[ $step -eq -1 ]]; then
 
-    log ">>>>> Validation data"
+    echo ============================================================================
+    echo "                          Data Preparation                                "
+    echo ============================================================================
+
+    echo ">>>>> Validate data"
 
     utils/validate_data_dir.sh --no-feats $data_dir/train
     utils/fix_data_dir.sh $data_dir/train
@@ -45,18 +47,18 @@ if [[ $step -eq 1 ]] || [[ $step -eq -1 ]]; then
     utils/validate_data_dir.sh --no-feats $data_dir/test
     utils/fix_data_dir.sh $data_dir/test
 
-    log ">>>>> Preparing dictionary"
+    echo ">>>>> Prepare dictionary"
 
     utils/prepare_lang.sh $data_dir/local/dict "<UNK>" $data_dir/local/lang $data_dir/lang || exit 1
 
-    log ">>>>> Preparing language model"
+    echo ">>>>> Prepare language model"
 
     lang=$data_dir/lang
     [[ ! -d $lang ]] && mkdir $lang
 
     arpa2fst --disambig-symbol=#0 $data_dir/lm/lm.arpa $lang/G.fst
 
-    log ">>>>> Extracting MFCC features"
+    eho ">>>>> Extract MFCC features"
 
     steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" $data_dir/train $output_dir/make_mfcc/train $output_dir/mfcc/train
     steps/compute_cmvn_stats.sh $data_dir/train $output_dir/make_mfcc/train $output_dir/mfcc/train
@@ -67,15 +69,18 @@ if [[ $step -eq 1 ]] || [[ $step -eq -1 ]]; then
     fi
 fi
 
-#### Stage 2 - Monophone training ####
+#### Step 2 - Monophone ####
 
 if [[ $step -eq 2 ]] || [[ $step -eq -1 ]]; then
 
-    log ">>>>> Monophone: training"
+    echo ============================================================================
+    echo "                          mono : Monophone                                "
+    echo ============================================================================
 
-    steps/train_mono.sh --boost-silence 1.25  --nj $nj --cmd "$train_cmd" $data_dir/train $data_dir/lang $data_dir/exp/mono || exit 1
+    echo ">>>>> Monophone: training"
 
-    # graph compilation
+    steps/train_mono.sh --nj $nj --cmd "$train_cmd" $data_dir/train $data_dir/lang $data_dir/exp/mono || exit 1
+
     utils/mkgraph.sh --mono $data_dir/lang $data_dir/exp/mono $data_dir/exp/mono/graph || exit 1
 
     echo ">>>>> Monophone: decoding"
@@ -83,64 +88,104 @@ if [[ $step -eq 2 ]] || [[ $step -eq -1 ]]; then
     steps/decode.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/mono/graph $data_dir/test $data_dir/exp/mono/decode_test || exit 1
 fi
 
-#### Stage 3 - Triphone - Delta training ####
+#### Step 3 - Deltas + Delta-Deltas ####
 
 if [[ $step -eq 3 ]] || [[ $step -eq -1 ]]; then
 
-    echo ">>>>> Monophone: aligment"
+    echo ============================================================================
+    echo "                       tri1 : Deltas + Delta-Deltas                       "
+    echo ============================================================================
 
-    steps/align_si.sh --boost-silence 1.25 --nj $nj  --cmd "$train_cmd" $data_dir/train $data_dir/lang $data_dir/exp/mono $data_dir/exp/mono_ali || exit 1
+    echo ">>>>> Monophone: alignment"
 
-    log ">>>>> Triphone - Delta: training"
+    steps/align_si.sh --boost-silence 1.25 --nj $nj  --cmd "$train_cmd" \
+        $data_dir/train $data_dir/lang $data_dir/exp/mono $data_dir/exp/mono_ali || exit 1
 
-    steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 1.25  4200 40000  $data_dir/train $data_dir/lang $data_dir/exp/mono_ali $data_dir/exp/tri1 || exit 1
+    echo ">>>>> Deltas + Delta-Deltas: training"
 
-    # graph compilation
+    steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 1.25  $numLeavesTri1 $numGaussTri1  \
+        $data_dir/train $data_dir/lang $data_dir/exp/mono_ali $data_dir/exp/tri1 || exit 1
+
     utils/mkgraph.sh $data_dir/lang $data_dir/exp/tri1 $data_dir/exp/tri1/graph
 
-    echo ">>>>> Triphone - Delta: decoding"
+    echo ">>>>> Deltas + Delta-Deltas: decoding"
 
     steps/decode.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/tri1/graph  $data_dir/test $data_dir/exp/tri1/decode_test || exit 1
 fi
 
-#### Stage 4 - Triphone - Delta + Delta-Delta training ####
+#### Step 4 - LDA-MLLT ####
 
 if [[ $step -eq 4 ]] || [[ $step -eq -1 ]]; then
 
-    echo ">>>>> Triphone - Delta: aligment"
+    echo ============================================================================
+    echo "                             tri2 : LDA-MLLT                              "
+    echo ============================================================================
 
-    steps/align_si.sh --boost-silence 1.25 --nj $nj --cmd "$train_cmd" $data_dir/train $data_dir/lang $data_dir/exp/tri1 $data_dir/exp/tri1_ali || exit 1
+    echo ">>>>> Deltas + Delta-Deltas: alignment"
 
-    log ">>>>> Triphone - Delta + Delta-Delta: training"
+    steps/align_si.sh --nj $nj --cmd "$decode_cmd" $data_dir/train $data_dir/lang $data_dir/exp/tri1 $data_dir/exp/tri1_ali || exit 1
 
-    steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 1.25  4200 40000  $data_dir/train $data_dir/lang $data_dir/exp/tri1_ali $data_dir/exp/tri2a || exit 1
+    echo ">>>>> LDA-MLLT: training"
 
-    # graph compilation
-    utils/mkgraph.sh $data_dir/lang  $data_dir/exp/tri2a $data_dir/exp/tri2a/graph || exit 1
+    steps/train_lda_mllt.sh --cmd "$train_cmd" $numLeavesMLLT $numGaussMLLT  \
+        $data_dir/train $data_dir/lang $data_dir/exp/tri1_ali $data_dir/exp/tri2 || exit 1
 
-    echo ">>>>> Triphone - Delta + Delta-Delta: decoding"
+    utils/mkgraph.sh $data_dir/lang  $data_dir/exp/tri2 $data_dir/exp/tri2/graph || exit 1
 
-    steps/decode.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/tri2a/graph  $data_dir/test $data_dir/exp/tri2a/decode_test || exit 1
+    echo ">>>>> LDA-MLLT: decoding"
+
+    steps/decode.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/tri2/graph  $data_dir/test $data_dir/exp/tri2/decode_test || exit 1
 fi
 
-#### Stage 5 - Triphone - LDA-MLLT training ####
+#### Step 5 - LDA-MLLT + SAT ####
 
 if [[ $step -eq 5 ]] || [[ $step -eq -1 ]]; then
 
-    echo ">>>>> Triphone - Delta + Delta-Delta: aligment"
+    echo ============================================================================
+    echo "                         tri3 : LDA-MLLT + SAT                            "
+    echo ============================================================================
 
-    steps/align_si.sh --boost-silence 1.25 --nj $nj --cmd "$decode_cmd" $data_dir/train $data_dir/lang $data_dir/exp/tri2a $data_dir/exp/tri2a_ali || exit 1
+    echo ">>>>> LDA-MLLT: alignment"
 
-    log ">>>>> Triphone - LDA-MLLT: training"
+    steps/align_si.sh --nj $nj --cmd "$decode_cmd" $data_dir/train $data_dir/lang $data_dir/exp/tri2 $data_dir/exp/tri2_ali || exit 1
 
-    steps/train_lda_mllt.sh --cmd "$train_cmd" --boost-silence 1.25  4200 40000  $data_dir/train $data_dir/lang $data_dir/exp/tri2a_ali $data_dir/exp/tri2b || exit 1
+    echo ">>>>> LDA-MLLT + SAT: training"
 
-    # graph compilation
-    utils/mkgraph.sh $data_dir/lang  $data_dir/exp/tri2b $data_dir/exp/tri2b/graph || exit 1
+    steps/train_sat.sh --cmd "$train_cmd" $numLeavesSAT $numGaussSAT  \
+        $data_dir/train $data_dir/lang $data_dir/exp/tri2_ali $data_dir/exp/tri3 || exit 1
 
-    echo ">>>>> Triphone - LDA-MLLT: decoding"
+    utils/mkgraph.sh $data_dir/lang  $data_dir/exp/tri3 $data_dir/exp/tri3/graph || exit 1
 
-    steps/decode.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/tri2b/graph  $data_dir/test $data_dir/exp/tri2b/decode_test || exit 1
+    echo ">>>>> LDA-MLLT + SAT: decoding"
+
+    steps/decode_fmllr.sh --nj $nj --cmd "$decode_cmd" $data_dir/exp/tri3/graph  $data_dir/test $data_dir/exp/tri3/decode_test || exit 1
+fi
+
+#### Step 6 - SGMM2  ####
+
+if [[ $step -eq 6 ]] || [[ $step -eq -1 ]]; then
+
+    echo ============================================================================
+    echo "                                 SGMM2                                    "
+    echo ============================================================================
+
+    echo ">>>>> LDA-MLLT + SAT: alignment"
+
+    steps/align_fmllr.sh --nj $nj --cmd "$decode_cmd" $data_dir/train $data_dir/lang $data_dir/exp/tri3 $data_dir/exp/tri3_ali || exit 1
+
+    echo ">>>>> SGMM2: training"
+
+    steps/train_ubm.sh --cmd "$train_cmd" $numGaussUBM $data_dir/train $data_dir/lang $data_dir/exp/tri3_ali $data_dir/exp/ubm4 || exit 1
+
+    steps/train_sgmm2.sh --cmd "$train_cmd" $numLeavesSGMM $numGaussSGMM \
+        $data_dir/train $data_dir/lang $data_dir/exp/tri3_ali $data_dir/ubm4/final.ubm $data_dir/exp/sgmm2_4 || exit 1
+
+    utils/mkgraph.sh $data_dir/lang $data_dir/exp/sgmm2_4 $data_dir/exp/sgmm2_4/graph || exit 1
+
+    echo ">>>>> SGMM2: decoding"
+
+    steps/decode_sgmm2.sh --nj $nj --cmd "$decode_cmd" --transform-dir \
+        $data_dir/exp/tri3/decode_test $data_dir/exp/sgmm2_4/graph $data_dir/train $data_dir/exp/sgmm2_4/decode_test || exit 1
 fi
 
 #### score
