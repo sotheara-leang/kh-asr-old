dbafbae7f4ea74124b85c3b97e0d59f04a4e208e
sotheara <leangsotheara@gmail.com>
2019-09-13 22:37:18 +0700

    refactor codes

--- local/prepare_data.py
+++ local/prepare_data.py
@@ -47,28 +47,38 @@ def generate_datasets(options):
 
         with open(train_dir + '/text', mode='w', encoding='utf-8') as text_writer, \
                 open(train_dir + '/wav.scp', mode='w', encoding='utf-8') as wav_writer, \
-                open(train_dir + '/utt2spk', mode='w', encoding='utf-8') as utt2spk_writer, \
-                open(train_dir + '/spk2utt', mode='w', encoding='utf-8') as spk2utt_writer:
+                open(train_dir + '/utt2spk', mode='w', encoding='utf-8') as utt2spk_writer:
+
+            invalid_examples = []
 
             for record in train_set:
+                wav_path = os.path.abspath(options.data_dir + '/wav/' + record[0] + '.wav')
+                if not os.path.exists(wav_path):
+                    print('!!!!! Wave file not found: %s' % wav_path)
+
+                    invalid_examples.append(record[0])
+                    continue
+
                 text_writer.write('%s %s\n' % (record[0], record[1].strip()))
-                wav_writer.write('%s %s\n' % (record[0], os.path.abspath(options.data_dir + '/wav/' + record[0] + '.wav')))
+                wav_writer.write('%s %s\n' % (record[0], wav_path))
 
             spk_id_pattern = 'KM-\\d{2}-\\w{1}-\\d{2}-\\d{5}'
             spk_id_regex = re.compile(spk_id_pattern)
 
             train_set = sorted(train_set, key=lambda record: tuple(re.findall(spk_id_regex, record[2])))
             for record in train_set:
-                utt2spk_writer.write('%s %s\n' % (record[0], record[2]))
+                utt_id = record[0]
+                if utt_id in invalid_examples:
+                    continue
 
-            #subprocess.call(['%s/utils/utt2spk_to_spk2utt.pl' % PROJ_HOME, '%s/utt2spk' % train_dir], stdout=spk2utt_writer)
+                utt2spk_writer.write('%s %s\n' % (utt_id, record[2]))
+
+        with open(train_dir + '/spk2utt', mode='w', encoding='utf-8') as spk2utt_writer:
+            subprocess.call(['%s/utils/utt2spk_to_spk2utt.pl' % PROJ_HOME, '%s/utt2spk' % (PROJ_HOME + '/' + train_dir)], stdout=spk2utt_writer)
 
         print(">>>>> Generate test data")
 
         # test set
-
-        if test_set is None or len(test_set) == 0:
-            return
 
         test_dir = options.output_dir + '/' + 'test'
         if not os.path.exists(test_dir):
@@ -76,10 +86,18 @@ def generate_datasets(options):
 
         with open(test_dir + '/text', mode='w', encoding='utf-8') as text_writer, \
                 open(test_dir + '/wav.scp', mode='w', encoding='utf-8') as wav_writer, \
-                open(test_dir + '/utt2spk', mode='w', encoding='utf-8') as utt2spk_writer, \
-                open(test_dir + '/spk2utt', mode='w', encoding='utf-8') as spk2utt_writer:
+                open(test_dir + '/utt2spk', mode='w', encoding='utf-8') as utt2spk_writer:
+
+            invalid_examples = []
 
             for record in test_set:
+                wav_path = os.path.abspath(options.data_dir + '/wav/' + record[0] + '.wav')
+                if not os.path.exists(wav_path):
+                    print('!!!!! Wave file not found: %s' % wav_path)
+
+                    invalid_examples.append(record[0])
+                    continue
+
                 text_writer.write('%s %s\n' % (record[0], record[1].strip()))
                 wav_writer.write('%s %s\n' % (record[0], os.path.abspath(options.data_dir + '/wav/' + record[0] + '.wav')))
 
@@ -88,9 +106,14 @@ def generate_datasets(options):
 
             test_set = sorted(test_set, key=lambda record: tuple(re.findall(spk_id_regex, record[2])))
             for record in test_set:
-                utt2spk_writer.write('%s %s\n' % (record[0], record[2]))
+                utt_id = record[0]
+                if utt_id in invalid_examples:
+                    continue
 
-            #subprocess.call(['%s/utils/utt2spk_to_spk2utt.pl' % PROJ_HOME, '%s/utt2spk' % test_dir], stdout=spk2utt_writer)
+                utt2spk_writer.write('%s %s\n' % (utt_id, record[2]))
+
+        with open(test_dir + '/spk2utt', mode='w', encoding='utf-8') as spk2utt_writer:
+            subprocess.call(['%s/utils/utt2spk_to_spk2utt.pl' % PROJ_HOME, '%s/utt2spk' % (PROJ_HOME + '/' + test_dir)], stdout=spk2utt_writer)
 
 def main():
     parser = argparse.ArgumentParser()
